fa7109d5c0769fd5f166ae3a943423d903bc86c4
andrew hudson <a.j.hudson@hotmail.com>
2020-06-17 15:13:43 +1000

    removed local vars I tested with

--- workstation/knife/bootstrap_node.sh
+++ workstation/knife/bootstrap_node.sh
@@ -4,12 +4,11 @@
 
 set -e
 
-node='chef_node' #DNS resolvable name of the node to target
-node_user='root' #The user on the node to authenticate as over SSH
-node_ssh_key='/node_key' #Path to SSH key used to authenticate with node
-node_name='node1-centos' #The name your node will use when registering with CHEF server
+node='' #DNS resolvable name of the node to target
+node_user='' #The user on the node to authenticate as over SSH
+node_ssh_key='' #Path to SSH key used to authenticate with node
+node_name='' #The name your node will use when registering with CHEF server
 
-#chef_node is defined during container runtime
 #Need an SSH key to connect to the node
 knife bootstrap $node \
     -U $node_user \
