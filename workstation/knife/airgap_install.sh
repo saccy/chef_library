dff151c9bd879a370a2e2b373790613b59c86722
andrew hudson <a.j.hudson@hotmail.com>
2020-07-10 18:38:48 +1000

    handle only centos

--- workstation/knife/airgap_install.sh
+++ workstation/knife/airgap_install.sh
@@ -5,9 +5,15 @@
 chef_server=''
 chef_client_pkg=''
 
-cd /tmp/
-curl -O http://${chef_server}:8080/${chef_client_pkg}
-yum localinstall -y ${chef_client_pkg}
+#TODO: add rhel 
+if [[ $(cat /etc/os-release | grep ^ID= | cut -d'=' -f2 | tr -d '"') != 'centos' ]]; then
+    cd /
+    curl -O http://${chef_server}:8080/${chef_client_pkg}
+    yum localinstall -y ${chef_client_pkg}
+else
+    echo "Unsupported OS"
+    exit 1
+fi
 
 #if ubuntu
 # dpkg -i chef_13.2.20-1_amd64.deb
