c5598920467cd8b076434a2895975056b779eeae
andrew hudson <a.j.hudson@hotmail.com>
2020-07-14 11:04:47 +1000

    handled more distros

--- workstation/knife/airgap_install.sh
+++ workstation/knife/airgap_install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-#TODO: case statement do distinguish between distros
-
 chef_server=''
-chef_client_pkg=''
+os=$(cat /etc/os-release | grep ^ID= | cut -d'=' -f2 | tr -d '"')
+os_ver=$(cat /etc/os-release | grep ^VERSION_ID= | cut -d'=' -f2 | tr -d '"')
 
-#TODO: add rhel 
-if [[ $(cat /etc/os-release | grep ^ID= | cut -d'=' -f2 | tr -d '"') =~ ^(centos$|rhel$) ]]; then
-    cd /
-    curl -O http://${chef_server}:8080/${chef_client_pkg}
-    yum localinstall -y ${chef_client_pkg}
-else
-    echo "Unsupported OS"
-    exit 1
-fi
-
-#if ubuntu
-# dpkg -i chef_13.2.20-1_amd64.deb
+case $os in
+    'centos'|'rhel'|'amzn')
+        chef_client_pkg='chef-16.2.73-1.el7.x86_64.rpm'
+        curl -O http://${chef_server}:8080/${chef_client_pkg}
+        yum localinstall -y $chef_client_pkg
+        ;;
+    'ubuntu'|'debian')
+        chef_client_pkg='chef_16.2.73-1_amd64.deb'
+        curl -O http://${chef_server}:8080/${chef_client_pkg}
+        dpkg -i $chef_client_pkg
+        ;;
+    'sles')
+        chef_client_pkg='chef-16.2.73-1.sles12.x86_64.rpm'
+        curl -O http://${chef_server}:8080/${chef_client_pkg}
+        zypper in -y $chef_client_pkg
+        ;;
+    *)
+        echo "Unsupported OS: $os"
+        exit 1
+        ;;
+esac
