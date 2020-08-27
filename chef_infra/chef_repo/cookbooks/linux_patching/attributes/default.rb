4feb2e1169c87f41ee136d9ceaf50f2955b7d876
andrew hudson <a.j.hudson@hotmail.com>
2020-08-27 13:54:50 +1000

    included audit cookbook attributes

--- chef_infra/chef_repo/cookbooks/linux_patching/attributes/default.rb
+++ chef_infra/chef_repo/cookbooks/linux_patching/attributes/default.rb
@@ -1,2 +1,12 @@
 # Specify the free storage for a partition in GB
 default['linux_patching']['var_partition_min_diskspace_required_in_GB'] = 10
+
+# audit cookbook attributes
+# default['audit']['reporter'] = 'chef-server-automate'
+# default['audit']['fetcher']  = 'chef-server'
+# default['audit']['profiles'] = [
+#   {
+#     name: 'DevSec Linux Patch Benchmark',
+#     compliance: 'admin/linux-patch-baseline',
+#   },
+# ]
